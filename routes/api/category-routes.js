cdbccd94f038501cdcae745d4f2095017986dd32
Donovan Walker <130865355+dreamsalotl@users.noreply.github.com>
2023-08-26 10:00:31 -0500

    added routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,117 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
+//---------------------8<-------------------------
+// This is the endpoint that the front-end will use to get all categories
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  }
+  );
 });
+
+//---------------------8<-------------------------
+// This is the endpoint that the front-end will use to get a single category by id
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with this id'});
+      return;
+    }
+    res.json(dbCategoryData);
+  }
+  )
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  );
 });
+
+//---------------------8<-------------------------
+// This is the endpoint that the front-end will use to create a new category
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  }
+  );
 });
+
+//---------------------8<-------------------------
+// This is the endpoint that the front-end will use to update a category by id
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData[0]) {
+      res.status(404).json({ message: 'No category found with this id'});
+      return;
+    }
+    res.json(dbCategoryData);
+  }
+  )
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  );
 });
 
+//---------------------8<-------------------------
+// This is the endpoint that the front-end will use to delete a category by id
+
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'No category found with this id'});
+      return;
+    }
+    res.json(dbCategoryData);
+  }
+  )
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  }
+  );
 });
 
 module.exports = router;
