9f17a8d64e873dc97df0148b20f70cfafb8d9e0d
Donovan Walker <130865355+dreamsalotl@users.noreply.github.com>
2023-08-26 10:16:28 -0500

    added routes for tags and updated index for models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+//---------------------8<-------------------------
+// These are the associations between the models
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
